f06bae5f5ad0e9b17f18769044230b06a6768154
Misha <misha@crispstudio.es>
2024-02-14 19:44:28 +0200

    feat: - add firebase config;

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDSmeTnv7JzyffGcwHqhL1TDV2Ul-PsumI",
+  authDomain: "cotaskify.firebaseapp.com",
+  projectId: "cotaskify",
+  storageBucket: "cotaskify.appspot.com",
+  messagingSenderId: "35721667450",
+  appId: "1:35721667450:web:9e98f53ead68b638296cc3"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
