09f1c4aca0c3789ca23ef9e5cbae581fe101099c
Misha <misha@crispstudio.es>
2024-02-15 19:04:04 +0200

    feat: - add Signup component layout and styles;

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,7 +1,51 @@
+import { useState } from 'react';
 import './Signup.css';
 
 export default function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const [thumbnail, setThumbnail] = useState(null);
+
   return (
-    <div>Signup</div>
+    <form className='auth-form'>
+      <h2>Sign up</h2>
+
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+
+      <label>
+        <span>display name:</span>
+        <input
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+
+      <label>
+        <span>profile thumbnail:</span>
+        <input
+          type="file"
+        />
+      </label>
+
+      <button className="btn">Sign up</button>
+    </form>
   )
 }
